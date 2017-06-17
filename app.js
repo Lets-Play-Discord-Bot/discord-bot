7660d676b1d0ae7344cf24f296651708ff5432d7
Lets Play <letsplaydiscordbot@gmail.com>
2017-06-17 13:04:47 +0200

    -app.js Code makes more sense now Modified how plugins receive parameters
    -plugins/pong/app.js
    Modified plugin structure

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ const discordBotConfig = require('./configs/bot_settings');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// Subscribe to events
 client.on('ready', () => {
   console.log('Bot connected successfully to the server.');
   LoadPlugins(client);
 });
 
-client.login(discordBotConfig.bot_token);
-
-
-function LoadPlugins(client){
+function LoadPlugins(client) {
   // Load plugins
-  require('./plugins/pong/app')(client);
+  console.log('Beginning to load plugins');
+
+  let pingPong = require('./plugins/pong/app')({
+    client: client
+  });
+  
+  console.log('All plugins loaded');
 }
+
+function Init() {
+  console.log('Beginning initialization');
+  client.login(discordBotConfig.bot_token);
+}
+
+// Start app
+Init();
--- plugins/pong/app.js
+++ plugins/pong/app.js
@@ -1,7 +1,14 @@
 const Discord = require('discord.js');
 
-module.exports = function (client) {
+function pong(options) {
+    // Make sure client is available
+    if (!options.client) {
+        throw new Error('Options.client is required');
+    }
 
+    let client = options.client;
+
+    // Listening for events
     client.on('message', message => {
         if (message.content === 'ping') {
             message.reply('pong');
@@ -11,4 +18,9 @@ module.exports = function (client) {
         }
     });
 
+
+    return {
+    }
 }
+
+module.exports = pong;
